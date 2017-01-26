6037304033607d4773cfda471bbd4980cfa19da3
jorgeucano <jorgeucano@gmail.com>
2017-01-26 07:23:58 -0500

    Typescript: interpolation

--- typescript/objetos.js
+++ typescript/objetos.js
@@ -2,9 +2,14 @@ var MyPerson = (function () {
     function MyPerson(first_name, last_name) {
         console.log("first_name", first_name);
         console.log("last_name", last_name);
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
+    MyPerson.prototype.getSaludo = function () {
+        var emojis = '(⌐■_■)';
+        return "Saludos \n            " + this.last_name + ", " + this.first_name + "\n            Le enviamos un saludo desde la consola!\n\n            " + emojis + "\n        ";
+    };
     return MyPerson;
 }());
-var personaUno = new MyPerson();
-var personaDos = new MyPerson('Jorge');
 var personaTres = new MyPerson('Jorge', 'Cano');
+console.log(personaTres.getSaludo());
--- typescript/objetos.ts
+++ typescript/objetos.ts
@@ -2,15 +2,25 @@ class MyPerson{
     first_name:string;
     last_name:string;
 
+
     constructor(first_name?:string, last_name?:string){
         console.log("first_name", first_name);
         console.log("last_name", last_name);
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
 
+    getSaludo():string{
+        let emojis = '(⌐■_■)';
+        return `Saludos 
+            ${this.last_name}, ${this.first_name}
+            Le enviamos un saludo desde la consola!
+
+            ${emojis}
+        `;
+    }
 
 }
-let personaUno = new MyPerson();
-
-let personaDos = new MyPerson('Jorge');
 
 let personaTres = new MyPerson('Jorge', 'Cano');
+console.log(personaTres.getSaludo());

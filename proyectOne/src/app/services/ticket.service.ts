9b715f83e69c0db5e5e6d95295269e45429d2eeb
jorgeucano <jorgeucano@gmail.com>
2017-01-31 08:16:04 -0500

    MEAN: Ticket update / delete Angular Service

--- proyectOne/src/app/services/ticket.service.ts
+++ proyectOne/src/app/services/ticket.service.ts
@@ -29,19 +29,49 @@ export class TicketService{
       return body || { };
   }
 
-    private handleError (error: Response | any) {
-      // en el mundo real, esto deberia ir a un log de archivo
-      let errMsg: string;
-      if (error instanceof Response) {
-        const body = error.json() || '';
-        const err = body.error || JSON.stringify(body);
-        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-      } else {
-        errMsg = error.message ? error.message : error.toString();
-      }
-      console.error(errMsg);
-      return Observable.throw(errMsg);
+  private handleError (error: Response | any) {
+    // en el mundo real, esto deberia ir a un log de archivo
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
     }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  updateTicketMongo(ticket: any): Promise<any> {
+  //let headers = new Headers({ 'Content-Type': 'application/json' });
+  //let options = new RequestOptions({ headers: headers });
+    return this.http.post(
+                  this.urlBackend+'ticketUpdate',
+                  { 
+                      'id': ticket.id,
+                      'titulo': ticket.titulo,
+                      'estado': ticket.estado
+                  }
+              )
+              .toPromise()
+              .then(this.extractData)
+              .catch(this.handleError);
+  }
+
+  deleteTicketMongo(ticket:any): Promise<any> {
+    //let headers = new Headers({ 'Content-Type': 'application/json' });
+    //let options = new RequestOptions({ headers: headers });
+    return this.http.post(
+                  this.urlBackend+'ticketDelete',
+                  { 
+                      'id': ticket.id
+                  }
+              )
+              .toPromise()
+              .then(this.extractData)
+              .catch(this.handleError);
+  }
 
 
     miVariableTicketGlobal = "soy una variable";
